48a8ee2169b4ce111661ff09e20f88b2788b173e
mohamedelbachir <mohamedelbachirboubanganadakou@gmail.com>
2023-07-07 07:49:27 +0200

    few update

--- src/App.js
+++ src/App.js
@@ -93,11 +93,16 @@ function App() {
               disabled={loading}
               onClick={handleClick}
             >
-              <img src="./images/icon-dice.svg" alt="dice" />
+              <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
+                  fill="#202733"
+                />
+              </svg>
             </button>
           </div>
           <div class="attribution">
-            Challenge by
+            Challenge by{" "}
             <a
               href="https://www.frontendmentor.io?ref=challenge"
               target="_blank"
@@ -105,7 +110,7 @@ function App() {
             >
               Frontend Mentor
             </a>
-            . Coded by
+            . Coded by{" "}
             <a
               href="https://github.com/mohamedelbachir"
               target="_blank"
